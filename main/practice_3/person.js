7cb86eb90426d3ddb24818b6b5588f561c2f8c35
Ryan.Wang <ranwang1995@outlook.com>
2020-01-22 23:10:06 +0800

    [WangRan] feat-1 finish practice1-3

--- main/practice_3/person.js
+++ main/practice_3/person.js
@@ -1,1 +1,11 @@
+class Person {
+  constructor(name, age, klass) {
+    this.name = name;
+    this.age = age;
+  }
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  }
+}
 
+module.exports = Person;
--- main/practice_3/worker.js
+++ main/practice_3/worker.js
@@ -1,1 +1,11 @@
+class Worker {
+  constructor(name, age, klass) {
+    this.name = name;
+    this.age = age;
+  }
+  introduce() {
+    return "I am a Worker. I have a job.";
+  }
+}
 
+module.exports = Worker;
