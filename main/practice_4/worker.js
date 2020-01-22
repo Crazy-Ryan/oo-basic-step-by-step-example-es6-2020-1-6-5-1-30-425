ea7626b711c724a58b3d1e08b975aaf34f51e69d
Ryan.Wang <ranwang1995@outlook.com>
2020-01-22 23:28:39 +0800

    [WangRan] feat-4: finish practice4

--- main/practice_4/worker.js
+++ main/practice_4/worker.js
@@ -1,1 +1,9 @@
+import Person from "../../main/practice_1/person.js";
+class Worker extends Person {
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Worker. I have a job.`;
+  }
+}
+
+module.exports = Worker;
 
