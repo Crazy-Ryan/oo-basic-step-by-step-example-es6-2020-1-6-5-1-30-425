ec6799aeda578a825d07338591544a02a846ef0a
Ryan.Wang <ranwang1995@outlook.com>
2020-01-27 00:08:17 +0800

    [WangRan] feat-8: finish practice8

--- main/practice_8/student.js
+++ main/practice_8/student.js
@@ -1,1 +1,18 @@
+import Person from "../../main/practice_8/person.js";
+class Student extends Person {
+  constructor(id, name, age, klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    let prefix = (new Person(this.id, this.name, this.age)).introduce() + ' ' + `I am a Student. `;
+    if (this.klass.leader === this) {
+      return prefix + `I am Leader of Class ${this.klass.number}.`
+    }
+    else {
+      return prefix + `I am at Class ${this.klass.number}.`
+    }
+  }
+}
 
+module.exports = Student;
--- main/practice_8/teacher.js
+++ main/practice_8/teacher.js
@@ -1,1 +1,19 @@
+import Person from "../../main/practice_8/person.js";
+class Teacher extends Person {
+  constructor(id, name, age, klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    let prefix = (new Person(this.id, this.name, this.age)).introduce() + ' I am a Teacher. ';
+    if (this.klass) {
+      return prefix + `I teach Class ${this.klass.number}.`;
+    }
+    else {
+      return prefix + `I teach No Class.`;
+    }
+  }
+}
+
+module.exports = Teacher;
 
